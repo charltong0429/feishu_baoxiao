d4de10159310922ac32f22d47648f29922bdb9dd
charltong0429 <ji.shaochen@outlook.com>
2026-06-30 21:56:40 +0200

    fix: refactor request.js to avoid new Promise(async) anti-pattern

--- miniapp/utils/request.js
+++ miniapp/utils/request.js
@@ -1,9 +1,9 @@
 import config from '../config';
 import { getToken } from './auth';
 
-export function request({ url, method = 'GET', data } = {}) {
-  return new Promise(async (resolve, reject) => {
-    const token = await getToken().catch(() => null);
+export async function request({ url, method = 'GET', data } = {}) {
+  const token = await getToken().catch(() => null);
+  return new Promise((resolve, reject) => {
     tt.request({
       url: config.BASE_URL + url,
       method,
